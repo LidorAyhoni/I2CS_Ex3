32a1db22313271183f2f4f3e493d81b6b769792b
Lidor <Lidorayhoni12@gmail.com>
2026-01-01 19:46:01 +0200

    Done Implement Index2D

--- src/Index2D.java
+++ src/Index2D.java
@@ -1,38 +1,74 @@
 package assignments.Ex3;
 
 public class Index2D implements Pixel2D {
-    private int _x, _y;
-    public Index2D() {this(0,0);}
-    public Index2D(int x, int y) {_x=x;_y=y;}
-    public Index2D(Pixel2D t) {this(t.getX(), t.getY());}
+    private final int __x, __y; //the object should not change(immutable)
+
+    /**
+     * Constructor of index in the matrix.
+     * @param x the X coordinate of the index.
+     * @param y the Y coordinate of the index.
+     */
+    public Index2D(int x, int y) {
+        this.__x =x;
+        this.__y =y;
+    }
+    /**Default constructor of index in the matrix.*/
+    public Index2D() {
+        this(0,0);
+    }
+
+    /**
+     * Copy constructor
+     * @throws RuntimeException if other==null.
+     * @param other index(point) to copy.
+     * */
+    public Index2D(Pixel2D other) {
+        if (other == null) throw new RuntimeException("other is null");
+        this.__x = other.getX();
+        this.__y = other.getY();
+    }
+    /**
+     * @return the X coordinate (integer) of the pixel.
+     */
     @Override
     public int getX() {
-        return _x;
+        return __x;
     }
+    /**
+     * @return the Y coordinate (integer) of the pixel.
+     */
     @Override
     public int getY() {
-        return _y;
+        return __y;
     }
-    public double distance2D(Pixel2D t) {
-        double ans = 0;
-        /////// add your code below ///////
-
-        ///////////////////////////////////
-        return ans;
+    /**
+     * This method computes the 2D (Euclidean) distance beteen this pixel and p2 pixel, i.e., (Math.sqrt(dx*dx+dy*dy))
+     * @throws RuntimeException if p2==null.
+     * @return the 2D Euclidean distance between the pixels.
+     */
+    @Override
+    public double distance2D(Pixel2D p2) {
+        if (p2 == null) throw new RuntimeException("p2 is null");
+        int deltaX = this.getX() - p2.getX();
+        int deltaY = this.getY() - p2.getY();
+        return Math.sqrt((double)deltaX * deltaX + (double)deltaY * deltaY);
     }
+    /**@return a String representation of this coordinate.*/
     @Override
     public String toString() {
         return getX()+","+getY();
     }
+    /**
+     * @param t the reference object with which to compare.
+     * @return true if and only if this Index is the same index as p.
+     */
     @Override
     public boolean equals(Object t) {
         boolean ans = false;
-       /////// you do NOT need to add your code below ///////
         if(t instanceof Pixel2D) {
             Pixel2D p = (Pixel2D) t;
             ans = (this.distance2D(p)==0);
         }
-       ///////////////////////////////////
         return ans;
     }
 }
